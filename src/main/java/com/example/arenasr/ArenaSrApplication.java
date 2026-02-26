fa89a5c5c96bc69190574e2ed4dd05016bae6ea6
Салпагаров Руслан <rusik12346rusik@gmail.com>
2026-02-26 20:13:16 +0300

    init

---
+++ src/main/java/com/example/arenasr/ArenaSrApplication.java
@@ -0,0 +1,13 @@
+package com.example.arenasr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArenaSrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArenaSrApplication.class, args);
+    }
+
+}
